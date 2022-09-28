81b8a3cd15d1ab44c1d80765a503f5b700b9811a
premj44 <premkumarjadhav9044@gmail.com>
2022-09-28 23:47:22 +0530

    added day-2 work

--- toggletrack/src/App.js
+++ toggletrack/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { ProjectBudgeting } from './components/project_budgeting/ProjectBudgeting';
+import Signup from './components/LoginSignup/Signup';
+// import { ProjectBudgeting } from './components/project_budgeting/ProjectBudgeting';
 
 function App() {
   return (
-    <div className="App">
-     
-     <ProjectBudgeting/>
+    <div className="App">   
+     {/* <ProjectBudgeting/> */}
+     <Signup/>
     </div>
   );
 }
