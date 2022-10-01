086790c2adf579e2a0a145d09ba7a8f92c4eaa29
vivek khade <khadev881@gmail.com>
2022-10-01 14:48:20 +0530

    modified

--- toggletrack/src/App.js
+++ toggletrack/src/App.js
@@ -1,5 +1,6 @@
 import AllRoutes from './Allroutes/AllRoutes';
 import './App.css';
+import LargeWithLogoCentered from './components/footer/footer';
 import Signup from './components/LoginSignup/Signup';
 import WithSubnavigation from './components/navbar/Navbar';
 // import { ProjectBudgeting } from './components/project_budgeting/ProjectBudgeting';
@@ -10,6 +11,7 @@ function App() {
       {/* <ProjectBudgeting/> */}
       <WithSubnavigation />
       <AllRoutes />
+      <LargeWithLogoCentered/>
       {/* <Signup/> */}
     </div>
   );
